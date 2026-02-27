060574c7c614f10fe1bd3a11438e8eef396b3562
Edgar Rafael Jr. <etrafael.dev@gmail.com>
2026-02-27 23:43:34 +0800

    feat: Add weapon system

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -2,15 +2,34 @@ class_name Player
 extends CharacterBody2D
 
 @onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
+@onready var weapon_root: Node2D = $WeaponRoot
+@onready var fire_rate_timer: Timer = $FireRateTimer
 
+var bullet_scene: PackedScene = preload("uid://0aqqgp0vrrdj")
 var input_player_authority: int
 
 const SPEED = 300.0
 
 func _ready():
-	player_input_synchronizer_component.set_multiplayer_authority(input_player_authority)
-	set_process(is_multiplayer_authority())
+	# only give control access depending on the player's peer id
+	player_input_synchronizer_component.set_multiplayer_authority(input_player_authority) 
 
-func _process(delta: float) -> void:
-	velocity = player_input_synchronizer_component.movement_vector*SPEED
-	move_and_slide()
+func _process(_delta: float) -> void:
+	# a specific player's weapon position pointing + mouse position
+	var aim_position = weapon_root.global_position + player_input_synchronizer_component.aim_vector
+	weapon_root.look_at(aim_position)
+	
+	if is_multiplayer_authority():
+		velocity = player_input_synchronizer_component.movement_vector*SPEED
+		move_and_slide()
+		if player_input_synchronizer_component.is_attack_pressed:
+			try_create_bullet()
+		
+func try_create_bullet():
+	if !fire_rate_timer.is_stopped():
+		return
+	var bullet = bullet_scene.instantiate() as Bullet
+	bullet.position = weapon_root.global_position
+	bullet.start(player_input_synchronizer_component.aim_vector)
+	get_parent().add_child(bullet, true)
+	fire_rate_timer.start()
---
+++ GameObjects/Bullets/bullet.gd
@@ -0,0 +1,22 @@
+class_name Bullet extends Node2D
+
+const SPEED: int = 600
+
+@onready var life_timer: Timer = $LifeTimer
+
+var direction: Vector2
+
+func _ready() -> void:
+	life_timer.timeout.connect(_on_life_timer_timeout)
+
+func _process(delta: float):
+	global_position += direction * SPEED * delta
+
+func start(direction: Vector2):
+	self.direction = direction
+	rotation = direction.angle()
+	
+func _on_life_timer_timeout():
+	# only free bullets on host side to prevent conflicts
+	if is_multiplayer_authority():
+		queue_free()
