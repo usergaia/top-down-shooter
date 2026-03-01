a888c8ee7224471ffe71ae76a086308134a0e95e
Edgar Rafael Jr. <etrafael.dev@gmail.com>
2026-03-01 22:02:59 +0800

    feat: Add round system

--- Manager/enemy_manager.gd
+++ Manager/enemy_manager.gd
@@ -1,17 +1,36 @@
 extends Node
 
+const ROUND_BASE_TIME: int = 10
+const ROUND_GROWTH: int = 5
+const BASE_ENEMY_SPAWN_TIME: float = 2.0
+const ENEMY_SPAWN_TIME_GROWTH: float = -.15
+
 @export var enemy_scene: PackedScene
 @export var enemy_spawn_root: Node
 @export var spawn_rect: ReferenceRect
 
 @onready var spawn_interval_timer: Timer = $SpawnIntervalTimer
+@onready var round_timer: Timer = $RoundTimer
+
+var round_count: int
 
 func _ready() -> void:
 	spawn_interval_timer.timeout.connect(_on_spawner_interval_timer_timeout)
+	round_timer.timeout.connect(_on_round_timer_timeout)
+	begin_round()
+
+func begin_round():
+	round_count += 1
+	round_timer.wait_time = ROUND_BASE_TIME + ((round_count-1) * ROUND_GROWTH)
+	round_timer.start()
+	
+	spawn_interval_timer.wait_time = BASE_ENEMY_SPAWN_TIME + ((round_count-1) * ENEMY_SPAWN_TIME_GROWTH)
+	spawn_interval_timer.start()
 	
 func _on_spawner_interval_timer_timeout():
 	if is_multiplayer_authority():
 		spawn_enemy()
+		spawn_interval_timer.start()
 	
 func spawn_enemy():
 	var enemy = enemy_scene.instantiate() as Node2D
@@ -22,3 +41,8 @@ func get_random_spawn_position() -> Vector2:
 	var x = randi_range(0, spawn_rect.size.x)
 	var y = randi_range(0, spawn_rect.size.y)
 	return spawn_rect.global_position + Vector2(x,y)
+
+func _on_round_timer_timeout():
+	if is_multiplayer_authority():
+		spawn_interval_timer.stop()
+		print("Round over!")
